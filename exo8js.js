852ff841f70c4e4d128ba873b242f1269262dce2
slecul <slecul@laposte.net>
2019-01-24 16:17:45 +0100

    après correction

--- exo8js.js
+++ exo8js.js
@@ -1,9 +1,23 @@
+// Déclaration de valeur.
 function valeur() {
+  //On récupère la saisie utilisateur.
   var age = document.getElementById('age').value;
-  if (age < 18) {
-    alert('Vous êtes mineur');
-  }
-  else {
-    alert('Vous êtes majeur');
+  // On vérifie que l'age entré est bien un nombre.
+  if(isNaN(age)){
+    alert('Vous n\'avez pas entré de nombre')
+    // Si c'est bien un nombre, on continue !
+  }else{
+    // On vérifie que l'age entré est cohérent.
+    if(age < 0 || age > 115){
+      alert('L\'âge entré ne correspond pas à un humain normalement constitué')
+    }else{
+      //On regarde si l'age est inférieur a 18, si c'est le cas, on dit qu'il est mineur.
+      if (age < 18) {
+        alert('Vous êtes mineur');
+        // Sinon, c'est qu'il est majeur.
+      }else{
+        alert('Vous êtes majeur');
+      }
+    }
   }
 }
